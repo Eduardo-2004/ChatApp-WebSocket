4dd698112af7a4c5caf0dc2ee62883fb4ce5b4e8
Eduardo <eduardoeusou1@gmail.com>
2023-03-24 12:00:57 -0300

    Add gitignore

--- server.js
+++ server.js
@@ -1,39 +1,67 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-const http = require("http").Server(app)
+const http = require("http").Server(app);
 
-const serverSocket = require("socket.io")(http)
+const serverSocket = require("socket.io")(http);
 
-const porta = 8000
+const porta = 8000;
 
 http.listen(porta, () => {
-    console.log("Servidor iniciado. Abra o navegador em http://localhost:" + porta);
+  console.log(
+    "Servidor iniciado na porta: " + porta
+  );
+});
 
-})
+app.get("/", (req, res) =>
+   res.sendFile(__dirname + '/index.html')
+);
 
-app.get("/", function(req, res){
-    res.sendFile(__dirname + '/index.html');
-})
+serverSocket.on('connection', (socket) => {
 
-serverSocket.on('connection', function(socket){
-
-    socket.on("login", function(nickname){
+    socket.on('login', function(nickname){
         console.log('Cliente conectado: ' + nickname);
-  
-        serverSocket.emit('chat msg', `Usuário ${nickname} conectou.`)
+        serverSocket.emit('chat msg', `Usuário ${nickname} conectou.`);
         socket.nickname = nickname
     })
 
     socket.on('chat msg', function(msg){
         console.log(`Msg recebida do cliente ${socket.nickname}: ${msg}`);
-        serverSocket.emit('chat msg', `${socket.nickname}: ${msg}`)
+        serverSocket.emit('chat msg', msg);
     })
 
-    socket.on('status', function(msg){
-        socket.broadcast.emit('status', msg)
-    })
+});
 
-})
+
+
+
+
+
+// serverSocket.on("connection", recebeConexaoUsuario);
+
+// function recebeConexaoUsuario(socket) {
+//   socket.on("login", (nickname) => registraLoginUsuario(socket, nickname));
+//   socket.on("disconnect", () =>
+//     console.log("Cliente desconectado: " + socket.nickname)
+//   );
+//   socket.on("chat msg", (msg) => encaminhaMsgsUsuarios(socket, msg));
+//   socket.on("status", (msg) => encaminhaMsgStatus(socket, msg));
+// }
+
+// function encaminhaMsgStatus(socket, msg) {
+//   socket.broadcast.emit("status", msg);
+// }
+
+// function encaminhaMsgsUsuarios(socket, msg) {
+//   console.log(`Msg recebida do cliente ${socket.nickname}: ${msg}`);
+//   serverSocket.emit("chat msg", `${socket.nickname}: ${msg}`);
+// }
+
+// function registraLoginUsuario(socket, nickname) {
+//   console.log("Cliente conectado: " + nickname);
+
+//   serverSocket.emit("chat msg", `Usuário ${nickname} conectou.`);
+//   socket.nickname = nickname;
+// }
